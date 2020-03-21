baeab92373ea6e3a4f9b8f92791dbdc28c52d190
Amrit <amritraj85@gmail.com>
2020-03-21 23:46:39 +0530

    my angular material app

---
+++ AngularMaterial/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  numbers = [];
+
+  constructor() {
+    for(let i=0; i<1000; i++) {
+      this.numbers.push(i);
+    }
+  }
+}
+
